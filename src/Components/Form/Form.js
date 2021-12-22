df326c61c9ec0e7481a2b8d0015bd38027a49b56
Choudhry-Shaharyar <sharry09@my.yorku.ca>
2021-12-21 19:45:49 -0500

    Added flexbox and media queries for responsiveness

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,8 +1,10 @@
 export function Form(props) {
     return (
         <form className="Form" onSubmit={props.onSubmit}>
+            <div class = "flex">
             <input type="text"placeholder="Enter a word.." value={props.word} onChange={props.onChange} />
             <input type="submit" value="Search"/>
+            </div>
         </form>
     );
 }
